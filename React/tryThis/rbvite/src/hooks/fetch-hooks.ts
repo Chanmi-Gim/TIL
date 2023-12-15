175acefc46420b4499f7d8049b590f301b6c5935
Chanmi-Gim <beatres@gachon.ac.kr>
2023-12-15 19:36:56 +0900

    refact : useEffect 활용하여 fetch-hooks 분리

---
+++ React/tryThis/rbvite/src/hooks/fetch-hooks.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    fetch(url, { signal })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+    return () => controller.abort();
+  }, []);
+  return data;
+};
--- React/tryThis/rbvite/src/hooks/session-context.tsx
+++ React/tryThis/rbvite/src/hooks/session-context.tsx
@@ -5,6 +5,7 @@ import {
   useEffect,
   useState,
 } from 'react';
+import { useFetch } from './fetch-hooks';
 
 type SessionContextProp = {
   session: Session;
@@ -31,16 +32,11 @@ export const SessionContextProvider = ({ children }: PropsWithChildren) => {
   const [session, setSession] = useState<Session>(DEFAULT_SESSION);
 
   const url = '/data/sample.json';
+  const data = useFetch<Session>(url);
   useEffect(() => {
-    const controller = new AbortController(); // AbortController: Promise 취소
-    const { signal } = controller;
-    fetch(url, { signal })
-      .then((res) => res.json())
-      .then((data) => setSession(data));
-    return () => {
-      controller.abort();
-    };
-  }, []);
+    if (data) setSession(data);
+    console.log('data: ', data);
+  }, [data]);
 
   const login = ({ id, name }: LoginUser) => {
     if (!name) {
