45594a7e473a0c35ff787627790399faeb7dfff7
Chanmi-Gim <beatres@gachon.ac.kr>
2023-12-15 17:23:58 +0900

    feat : useEffect 활용한 timer-hooks

--- React/tryThis/rbvite/src/App.tsx
+++ React/tryThis/rbvite/src/App.tsx
@@ -1,8 +1,15 @@
-import { forwardRef, useImperativeHandle, useRef, useState } from 'react';
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import './App.css';
 import Hello, { GiftHandle } from './components/Hello';
 import My from './components/My';
 import { useCounter } from './hooks/counter-context';
+import { useTimer } from './hooks/timer-hooks';
 
 type ChildHandler = {
   appendPeriod: () => void;
@@ -20,6 +27,26 @@ function App() {
   const { count, plusCount } = useCounter();
   const childRef = useRef<ChildHandler>(null);
   const giftHandleRef = useRef<GiftHandle>(null);
+  const [badCount, setBadCount] = useState(0);
+  const [goodCount, setGoodCount] = useState(0);
+  const { useInterval } = useTimer();
+
+  useInterval(() => setBadCount((pre) => pre + 1), 1000);
+
+  useEffect(() => {
+    const intl = setInterval(() => setGoodCount((pre) => pre + 1), 1000);
+    return () => clearInterval(intl);
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('>>useEffect');
+      setBadCount(100);
+      setGoodCount(100);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <button onClick={() => giftHandleRef.current?.getGift()}>
@@ -32,7 +59,8 @@ function App() {
         ref={giftHandleRef}
       ></Hello>
       <h2 style={{ color: 'skyblue' }}>your Count is "{count}"</h2>
-
+      <strong style={{ float: 'left', color: 'red' }}>{badCount}</strong>
+      <strong style={{ float: 'right', color: 'green' }}>{goodCount}</strong>
       <ChildComponent ref={childRef} />
       <div>
         <button onClick={() => childRef.current?.appendPeriod()}>
@@ -44,5 +72,4 @@ function App() {
     </>
   );
 }
-// 11월 10~ 12월 9일
 export default App;
---
+++ React/tryThis/rbvite/src/hooks/timer-hooks.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+export const useTimer = () => {
+  const useInterval = <T extends unknown[]>(
+    cb: (...args: T) => void,
+    delay: number
+  ) => {
+    useEffect(() => {
+      const timer = setInterval(cb, delay);
+      return () => clearInterval(timer);
+    }, []);
+  };
+  return { useInterval };
+};
